4531ca6527db9c3a3662b40cc513618da8788152
Robson Marcolino <robsonmarcolino@gmail.com>
2025-12-27 15:46:29 -0300

    chore: Vercel compatibility support

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/Nexus-store/",
+    base: process.env.VERCEL ? "/" : "/Nexus-store/",
 })
